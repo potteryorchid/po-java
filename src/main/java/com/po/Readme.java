7dc8249423e08f58d173a8dd16398507377afcc4
ZJ <zhangjie@megvii.com>
2018-08-10 12:17:00 +0800

    add readme

---
+++ src/main/java/com/po/Readme.java
@@ -0,0 +1,63 @@
+package com.po;
+
+/**
+ * JDK 1.8 jar 包说明
+ *
+ * /Home/lib 目录下文件
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ * drwxrwxr-x  14 root  wheel   476B Jul 12  2017 .
+ * drwxrwxr-x  16 root  wheel   544B Jul 12  2017 ..
+ * -rwxrwxr-x   1 root  wheel   1.1M Jun 27  2017 ant-javafx.jar
+ * -rw-rw-r--   1 root  wheel    17M Jul 12  2017 ct.sym
+ * -rw-rw-r--   1 root  wheel   159K Jul 12  2017 dt.jar
+ * -rw-rw-r--   1 root  wheel    18K Jul 12  2017 ir.idl
+ * -rwxrwxr-x   1 root  wheel    35K Jun 27  2017 javafx-mx.jar
+ * -rw-rw-r--   1 root  wheel   398K Jul 12  2017 jconsole.jar
+ * drwxrwxr-x  13 root  wheel   442B Jul 12  2017 missioncontrol
+ * -rw-rw-r--   1 root  wheel   640B Jul 12  2017 orb.idl
+ * -rwxrwxr-x   1 root  wheel   4.5K Jun 27  2017 packager.jar
+ * -rw-rw-r--   1 root  wheel   2.3M Jul 12  2017 sa-jdi.jar
+ * -rw-rw-r--   1 root  wheel    17M Jul 12  2017 tools.jar
+ * drwxrwxr-x   6 root  wheel   204B Jan 15  2017 visualvm
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ *
+ * /Home/jre/lib 目录下 jar 文件
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ * -rw-rw-r--  1 root  wheel   3.0M Jul 12  2017 lib/charsets.jar
+ * -rw-rw-r--  1 root  wheel   2.1M Jul 12  2017 lib/deploy.jar
+ * -rw-rw-r--  1 root  wheel   465K Jul 12  2017 lib/javaws.jar
+ * -rw-rw-r--  1 root  wheel   114K Jul 12  2017 lib/jce.jar
+ * -rw-rw-r--  1 root  wheel   547K Jul 12  2017 lib/jfr.jar
+ * -rwxrwxr-x  1 root  wheel    33K Jun 27  2017 lib/jfxswt.jar
+ * -rw-rw-r--  1 root  wheel   634K Jul 12  2017 lib/jsse.jar
+ * -rw-rw-r--  1 root  wheel   381B Jul 12  2017 lib/management-agent.jar
+ * -rw-rw-r--  1 root  wheel   920K Jul 12  2017 lib/plugin.jar
+ * -rw-rw-r--  1 root  wheel   3.3M Jul 12  2017 lib/resources.jar
+ * -rw-rw-r--  1 root  wheel    64M Jul 12  2017 lib/rt.jar
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ *
+ *
+ *
+ *
+ * {Java_Home}/lib/ 下 jar 包说明
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ * 1、dt.jar 是关于运行环境的类库，主要是swing的包，在用到swing时最好加上。
+ * 2、tools.jar 是工具类库，编译和运行需要的都是 toos.jar，即执行javac的时候用到，里面的类分别是 sun.tools.java.*; sun.tools.javac.*;
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ *
+ *
+ *
+ *
+ * {Java_Home}/jre/lib/ 下 jar 包说明
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ * 1、rt.jar 是JAVA基础类库，也就是你在java doc里面看到的所有的类的class文件。rt.jar 默认就在 Root Classloader 的加载路径里面的，而在 Classpath 配置该变量是不需要的。
+ * 同时 jre/lib 目录下的其他jar: jce.jar、jsse.jar、charsets.jar、resources.jar 都在 Root Classloader中。
+ * = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+ *
+ *
+ *
+ * Created by ZJ on 10/08/2018.
+ */
+public class Readme {
+
+}
