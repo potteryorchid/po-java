67c3233bef637f5145e7caae0b3903f1d30d14a1
ZJ <zhangjie@megvii.com>
2018-08-21 17:56:09 +0800

    add MaxSubSum and BinarySearch

---
+++ src/main/java/com/po/sd/algorithm/MaxSubSum.java
@@ -0,0 +1,38 @@
+package com.po.sd.algorithm;
+
+/**
+ * Created by ZJ on 21/08/2018.
+ */
+public class MaxSubSum {
+
+  /**
+   * 在数组中查找连续的一个子序列数组，这个子序列数组满足：若对其求和，和数是所有连续子序列数组中最大的。
+   *
+   * @param dts 传入数组参数
+   * @return 数组中 Sum 求和最大的连续子序列
+   */
+  public static double getMaxSubSum(double[] dts) {
+
+    double maxSum = 0.0;
+    double tmpSum = 0.0;
+
+    for (double dt : dts) {
+      tmpSum += dt;
+      if (tmpSum < 0) {
+        tmpSum = 0.0;
+      } else {
+        maxSum = tmpSum;
+      }
+    }
+    return maxSum;
+  }
+
+  public static void main(String[] args) {
+
+    double[] dts = new double[]{
+        1.0, -2.3, 3.4, 5, 6, -12, 76, 23
+    };
+
+    System.out.println(MaxSubSum.getMaxSubSum(dts));
+  }
+}
---
+++ src/main/java/com/po/sd/sorting/BinarySearch.java
@@ -0,0 +1,44 @@
+package com.po.sd.sorting;
+
+/**
+ * Created by ZJ on 21/08/2018.
+ */
+public class BinarySearch {
+
+  /**
+   * 二分查找
+   * 时间：O(log N)
+   * 条件：二分查找适用于有序的集合
+   * 原理：查找先取中间点数，若中间数就是要查找数，则返回，若中间数比要查找数大，则要查找的数在集合左边，反之在右边。不断重复该逻辑查找，直到跳出循环。
+   *
+   * @param arr 查找数据集合
+   * @param con 要查找的数
+   * @param <AnyType> 泛型参数，表示参数可以是任意实现继承 Comparable 的类型
+   */
+  public static <AnyType extends Comparable<? super AnyType>> int binarySearch(AnyType[] arr,
+      AnyType con) {
+
+    int low = 0, high = arr.length - 1;
+
+    while (low <= high) {
+      int mid = (low + high) / 2;
+
+      if (con.compareTo(arr[mid]) < 0) {
+        high = mid - 1;
+      } else if (con.compareTo(arr[mid]) > 0) {
+        low = mid + 1;
+      } else {
+        return mid;
+      }
+    }
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    Integer[] d = new Integer[]{2, 5, 12, 56, 102, 870, 1923};
+
+    System.out.println(BinarySearch.binarySearch(d, 54));
+  }
+
+}
