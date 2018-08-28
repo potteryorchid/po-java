5a6a2bde0c32b82b70b82fab6a460574df3865db
ZJ <zhangjie@megvii.com>
2018-08-28 17:49:05 +0800

    update BitOper add bit reserve

--- src/main/java/com/po/sd/algorithm/BitOper.java
+++ src/main/java/com/po/sd/algorithm/BitOper.java
@@ -1,5 +1,7 @@
 package com.po.sd.algorithm;
 
+import org.apache.poi.ss.formula.functions.T;
+
 /**
  * ####计算机是以补码的方式进行存储数据####
  * 原码表示法规定：用符号位和数值表示带符号数，正数的符号位用“0”表示，负数的符号位用“1”表示，数值部分用二进制形式表示。
@@ -139,6 +141,11 @@ public class BitOper {
   }
 
   /**
+   * Bits array operation:
+   * com.po.sd.algorithm.BitOper#createBitsByteArray(int)
+   * com.po.sd.algorithm.BitOper#setBitsTo1(byte[], int)
+   * com.po.sd.algorithm.BitOper#checkBitsIs1(byte[], int)
+   *
    * 根据长度创建一个存放 bits 的 byte[]，默认 bits 初始化为 0，可以替代 boolean[]
    *
    * @param length ：二进制数 bits 的长度
@@ -177,6 +184,62 @@ public class BitOper {
       return true;
     }
     return false;
+  }
+
+  /**
+   * int 转 二进制字符串
+   *
+   * @param in input value
+   * @return String result
+   */
+  public static String toBinaryStr(int in) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 31; i >= 0; i--) {
+      if (((in >> i) & 1) == 1) {
+        sb.append('1');
+      } else {
+        sb.append('0');
+      }
+      if (i == 16) {
+        sb.append(' ');
+      }
+    }
+    return sb.toString();
+  }
+
+  /**
+   * 高低位交换，适用于无符号数
+   *
+   * @param in Input value
+   * @return String result
+   */
+  public static int exchangePostion(int in) {
+    return (in >>> 16) | (in << 16);
+  }
+
+  /**
+   * 二进制反转顺序，将每位 0 or 1 的顺序做逆序操作。
+   * 1、先获取奇数位和偶数位上的二进制数，将奇数位上的二进制数左移，偶数位上的二进制数右移，然后两数求或；
+   * 2、以两位二进制数为单位，重复第一步；
+   * 3、以 2^2 位二进制数为单位，重复第一步；
+   * 4、以 2^3 位二进制数为单位，重复第一步；
+   * 5、以 2^n (2^n 为传入数据二进制数的位数的 1/2) 位二进制数为单位，重复第一步；
+   *
+   * 0x55555555 = 0101010101010101 0101010101010101
+   * 0x33333333 = 0011001100110011 0011001100110011
+   * 0x0f0f0f0f = 0000111100001111 0000111100001111
+   * 0xff00 = 0000000000000000 1111111100000000
+   *
+   * @param in input value
+   * @return int result
+   */
+  public static int bitsReverse(int in) {
+    in = (in & 0x55555555) << 1 | (in >>> 1) & 0x55555555;
+    in = (in & 0x33333333) << 2 | (in >>> 2) & 0x33333333;
+    in = (in & 0x0f0f0f0f) << 4 | (in >>> 4) & 0x0f0f0f0f;
+    in = (in << 24) | ((in & 0xff00) << 8) |
+        ((in >>> 8) & 0xff00) | (in >>> 24);
+    return in;
   }
 
   public static void main(String[] args) {
@@ -220,9 +283,9 @@ public class BitOper {
     BitOper.setBitsTo1(bytes, 3);
     BitOper.setBitsTo1(bytes, 23);
     BitOper.setBitsTo1(bytes, 93);
-    System.out.println(BitOper.checkBitsIs1(bytes,24));
-    System.out.println(BitOper.checkBitsIs1(bytes,23));
+    System.out.println(BitOper.checkBitsIs1(bytes, 24));
+    System.out.println(BitOper.checkBitsIs1(bytes, 23));
 
-    
+
   }
 }
